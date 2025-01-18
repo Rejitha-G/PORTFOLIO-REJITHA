84c24622871d978a59ebc56c57eb13b4da6ee4bb
Rejitha-G <rejithamolgs@gmail.com>
2025-01-18 20:06:26 +0530

    Add files via upload

---
+++ portfoliojs.js
@@ -0,0 +1,52 @@
+
+document.getElementById('i-about-me').addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('about-me-centent').scrollIntoView({ behavior:'smooth'});
+});
+
+
+document.getElementById('i-contact').addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('contact-session').scrollIntoView({ behavior:'smooth'});
+});
+
+
+document.getElementById('i-projects').addEventListener('click',function(e){
+  e.preventDefault();
+  document.getElementById('project-session').scrollIntoView({ behavior:'smooth'});
+});
+
+document.getElementById('skills').addEventListener('click',function(e){
+e.preventDefault();
+document.getElementById('skills-session').scrollIntoView({ behavior:'smooth'});
+});
+
+
+document.getElementById('contact-button').addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('contact-session').scrollIntoView({ behavior:'smooth'});
+    });
+
+
+    
+document.getElementById('about-me-f').addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('about-me-centent').scrollIntoView({ behavior:'smooth'});
+});
+
+
+document.getElementById('contact-f').addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('contact-session').scrollIntoView({ behavior:'smooth'});
+});
+
+
+document.getElementById('projects-f').addEventListener('click',function(e){
+  e.preventDefault();
+  document.getElementById('project-session').scrollIntoView({ behavior:'smooth'});
+});
+
+document.getElementById('skills-f').addEventListener('click',function(e){
+e.preventDefault();
+document.getElementById('skills-session').scrollIntoView({ behavior:'smooth'});
+});
